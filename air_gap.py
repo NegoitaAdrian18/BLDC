fd70b8cfa59d55c0589bce09553b2a1fd6be3387
negoitaadrian94@gmail.com <negoitaadrian94@gmail.com>
2022-11-23 20:57:04 +0200

    a

---
+++ air_gap.py
@@ -0,0 +1,7 @@
+from stator import Stator
+
+
+class AirGap(Stator):
+
+    def __init__(self):
+        super().__init__()
---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+import air_gap
+from stator import Stator
+
+bldc_motor = Stator()
+rez1 = bldc_motor.magnetic_flux_per_pole()
+print(rez1)
